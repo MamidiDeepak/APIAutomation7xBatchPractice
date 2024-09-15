a8fca1891c4e1de93a54e1f97a16c76e9f104953
DeepakQA <mamedideepak@gmail.com>
2024-09-15 23:44:19 +0530

    added the path and query param scripts

---
+++ src/test/java/com/APIAutomation7xBatchPractice/ThirdAPITestPathAndQueryParams.java
@@ -0,0 +1,23 @@
+package com.APIAutomation7xBatchPractice;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class ThirdAPITestPathAndQueryParams {
+
+//     https://reqres.in/api/users?page=2&id=7
+
+    @Test
+    void ValidatePathAndQueryParams(){
+
+            given()
+                    .pathParams("myPath","users")
+                    .queryParams("page",2)
+                    .queryParams("id",7)
+                    .when()
+                    .get("https://reqres.in/api/{myPath}")
+                    .then()
+                    .statusCode(200)
+                    .log().all();
+    }
+}
