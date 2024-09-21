4e9d24ba38b6db5327d9dc0f154cd6fe79abaca2
DeepakQA <mamedideepak@gmail.com>
2024-09-21 23:44:44 +0530

    added the api chaining scripts of sep-21st day

---
+++ src/test/java/com/APIAutomation7xBatchPractice/sep21st/todayClassPractice/DeleteBooking.java
@@ -0,0 +1,32 @@
+package com.APIAutomation7xBatchPractice.sep21st.todayClassPractice;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class DeleteBooking {
+
+
+    RequestSpecification varDeleteGiven = RestAssured.given();
+    @Test
+    public void deleteCreatedBooking(){
+
+        CreateToken ct = new CreateToken();
+        String token = ct.createTokenMethod();
+
+        CreateBooking createdBookingIdInUpdate = new CreateBooking();
+        int id = createdBookingIdInUpdate.createBooking();
+
+        varDeleteGiven.baseUri("https://restful-booker.herokuapp.com");
+        varDeleteGiven.basePath("/booking/"+id);
+//        varUpdateGiven.header("Authorization", "Bearer "+token);
+
+//        varUpdateGiven.contentType(ContentType.JSON);
+        varDeleteGiven.cookie("token",token);
+
+        Response res = varDeleteGiven.when().delete();
+
+        res.then().log().all().statusCode(201);
+    }
+}
