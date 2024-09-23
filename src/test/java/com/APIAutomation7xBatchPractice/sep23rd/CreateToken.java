c3f57724b8484819a34da7804dc1ff4892e1614b
Mamidideepak <deepak.mamidi@mymoneykarma.com>
2024-09-23 16:07:04 +0530

    added Create Token file with xml file

---
+++ src/test/java/com/APIAutomation7xBatchPractice/sep23rd/CreateToken.java
@@ -0,0 +1,57 @@
+package com.APIAutomation7xBatchPractice.sep23rd;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CreateToken {
+
+    RequestSpecification varGiven = RestAssured.given();
+
+    String tokenPayload = "{\n" +
+            "    \"username\" : \"admin\",\n" +
+            "    \"password\" : \"password123\"\n" +
+            "}";
+
+    @Test
+    public void createToken(){
+
+        varGiven.baseUri("https://restful-booker.herokuapp.com");
+        varGiven.basePath("/auth");
+        varGiven.contentType("application/json");
+        varGiven.body(tokenPayload);
+
+        Response response = varGiven.when().post();
+
+        String actualToken = response.jsonPath().getString("token");
+
+        System.out.println("Generated token is : >>> " +actualToken);
+
+            int statusCode = response.statusCode();
+            assertThat(statusCode).isEqualTo(200);
+            System.out.println("Status Code is : "+statusCode);
+
+            String statusText = response.statusLine();
+            assertThat(statusText).isEqualTo("HTTP/1.1 200 OK");
+            System.out.println("Status Text is : "+statusText);
+
+            long time = response.getTime();
+            assertThat(time).isBetween(1000L,3000L);
+            System.out.println("Response Time in ms : "+time);
+
+            String contentData = response.getHeader("Content-Type");
+            assertThat(contentData).isEqualTo("application/json; charset=utf-8");
+            System.out.println("Header is : "+contentData);
+
+            ResponseBody responseBody = response.body();
+            System.out.println("Response Body is: "+responseBody.asString());
+
+            assertThat(actualToken).isNotNull().isNotBlank();
+
+    }
+}
